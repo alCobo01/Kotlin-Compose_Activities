521bd4865d404a91ecfe848767f5bdd59a008b18
alCobo01 <alvaro.cobo.7e6@itb.cat>
2025-02-05 13:01:15 +0100

    Feina classe 05/02

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/trivial/Trivial.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/trivial/Trivial.kt
@@ -1,27 +1,37 @@
 package cat.itb.m78.exercices.trivial
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.VeniteAdoremus
 import m78exercices.composeapp.generated.resources.trivialLogo
 import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.painterResource
+
 
 
 
@@ -72,13 +82,31 @@ fun MenuScreen(navigateToGameScreen: () -> Unit, navigateToSettingsScreen: () ->
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().background(Color(0xFFF1C40F))
     ) {
+        Text(
+            text = "Trivial",
+            fontSize = 60.sp,
+            fontFamily = FontFamily(Font(Res.font.VeniteAdoremus))
+        )
         Image(
             painter = painterResource(Res.drawable.trivialLogo),
             contentDescription = null,
-            modifier = Modifier.size(150.dp).clip(CircleShape)
+            modifier = Modifier.size(200.dp).clip(CircleShape)
         )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(
+            onClick = navigateToGameScreen,
+            modifier = Modifier.height(300.dp).width(300.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3498DB)))
+        { Text("Game") }
+
+        Spacer(modifier = Modifier.padding(3.dp))
+
+        Button(
+            onClick = navigateToSettingsScreen,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF3498DB))) { Text("Settings") }
     }
 }
 
